f35c4a8edc16d68c58c4ccaa7be495d0aceeff3b
AhmedAlwaqidi <ahmedcomahmedocm@gmail.com>
2026-08-29 03:14:48 +0300

    fix: ensure button has a default no-op function when onPressed is null

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -8,7 +8,7 @@ class Button extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
+      onPressed: onPressed ?? () => {},
       child: child ?? Text('Clickable Button'),
     );
   }
